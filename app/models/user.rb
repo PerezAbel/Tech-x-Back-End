9f15ff32f9e9d3c931ca588687f59aa0a875235d
belammuia <belammuia0@gmail.com>
2024-10-27 20:47:49 +0300

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username,:email, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 4 }
 end
+
